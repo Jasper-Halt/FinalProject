df921c600e376c7854ef09dd5f9839d3e16afe65
Jasper-Halt <jphalt@aol.com>
2022-12-07 05:54:39 -0700

    added components for project

---
+++ src/components/sliderImage.js
@@ -0,0 +1,27 @@
+import SilverCity from "../assets/Silver.jpg";
+import Museum from "../assets/Museum.jpg";
+import AdobeSprings from "../assets/AdobeSprings.jpg";
+import ContinentalDivide from "../assets/ContinentalDivide.jpg"
+import WNMU from "../assets/WNMU.jpg"
+import Downtown from "../assets/Downtown.jpg"
+
+export default [
+  {
+    urls: SilverCity,
+  },
+  {
+    urls: Museum,
+  },
+  {
+    urls: AdobeSprings,
+  },
+  {
+    urls: ContinentalDivide,
+  },
+  {
+    urls: WNMU,
+  },
+  {
+    urls: Downtown,
+  },
+];
